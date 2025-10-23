8f76780fafb56440ba20f68dc914a262513ed9eb
shahadaljaeethin <shahadaljaeethin@gmail.com>
2025-10-23 11:24:58 +0300

    interface

---
+++ src/main.java
@@ -0,0 +1,50 @@
+import java.util.Scanner;
+
+public class main {
+    public static void main(String[] args){
+    Scanner r = new Scanner(System.in);
+    print("Welcome to  Tic Tac Toe game!\nyou will play againts the computer.\n");
+    String choice = "Y";
+//---------------------------------------------------------
+    while(true){
+    try {
+        print("Type (Y/y) to start a game: ");
+        choice = r.next();
+        if (!choice.equalsIgnoreCase("y")) break;
+        startGame();
+    }catch (Exception e){}
+    }
+    print("\nHope you enjoyed the game.");
+    }//main
+
+    public static void printBoard(char[][] board){
+    int colmn = 0;
+        for(int i=0;i<board.length;i++) //rows
+         {
+             for(int c=colmn;c<(colmn+3);c++)  //colmn
+                 print("[" + board[i][c] + "]");
+
+         print("\n-----------\n");
+
+         }
+
+    }
+public static void startGame(){
+    //create new board.
+    char[][] board = {
+            {'1', '2', '3'}, //board[0] = row #1      [0][1][2] colmn
+            {'4', '5', '6'}, //board[1] = row #2
+            {'7', '8', '9'}  //board[2] = row #3
+    };
+
+
+    printBoard(board);
+
+}
+
+
+    //method for printing shortcut
+    static void print(String message){
+    System.out.print(message);
+    }
+}
