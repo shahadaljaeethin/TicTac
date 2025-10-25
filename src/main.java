7a2e8a37d96bc9da22067e20c2be74bc86915bdd
shahadaljaeethin <shahadaljaeethin@gmail.com>
2025-10-25 10:53:17 +0300

    main game parts complete

--- src/main.java
+++ src/main.java
@@ -37,6 +37,7 @@ public class main {
 ========================================================================================
 */
 public static void startGame(){
+char someoneWon ='N';
     //1.create new board.
     char[][] board = {
             {'1', '2', '3'}, //board[0] = row #1      [0][1][2] colmn
@@ -44,15 +45,15 @@ public static void startGame(){
             {'7', '8', '9'}  //board[2] = row #3
     };
     boolean haveWinner=false; //means whether X/O has won.
-    while (true) {
+    while (someoneWon=='N') {
         //2.show the board
         printBoard(board);
         //3.take the input
        board = updateBoard(board);
         //4.check the winner
-                 }
+        someoneWon= haveWinner(board);   }
         //5.return a score!
-
+        print(someoneWon+" has won!\n");
 }
 
 public static void printBoard(char[][] board){
@@ -80,9 +81,9 @@ char[][] newBoard = old;
 Scanner r = new Scanner(System.in);
 int index,row,colmn;
 int[] pairs;
-Random computer = new Random();
-        print("\n***(player X's turn)***");
-        print("\n===================\n");
+Random random = new Random();
+        print("***(Your turn)***");
+        print("\n===================");
 
     //1.ask the player
         while(true) {
@@ -100,14 +101,47 @@ Random computer = new Random();
         }
         catch (InputMismatchException e){print("\nindex must be numbers from the board.\n"); r.nextLine();}
         catch (IndexOutOfBoundsException e){print("\nError in array reading.\n");}
-        catch (Exception e){print("\nINDEX ERROR: "+e.getMessage()); }
+        catch (Exception e){print("\nINDEX ERROR: "+e.getMessage()); r.nextLine(); }
         }
         //2.play the computer
-        print("\n***(player O's turn)***\n");
-        print("\n===================\n");
+        while(true){
+        try{
+        index =  random.nextInt(9) +1;
+        isEmpty(index,newBoard);//may throw exception if the index is taken
+        pairs =  getPair(index);//won't throw since limit always 1-9.
+        row = pairs[0];
+        colmn = pairs[1];
+        newBoard[row][colmn]='O';
+        break;
+        }catch (Exception e){}//don't show a message just re-choice again.
+        }
         return newBoard;
 
 }
+
+public static char haveWinner(char[][] board){
+char win = 'N';//N = nobody , X = x won , O = o won.
+for(int r=0;r<3;r++){   //winnig in Rows
+    if(board[r][0]=='X'&&board[r][1]=='X'&&board[r][2]=='X')  win = 'X';
+    else if(board[r][0]=='O'&&board[r][1]=='O'&&board[r][2]=='O')  win = 'O';
+}
+for(int c=0;c<3;c++){   //winning in Colmns
+    if(board[0][c]=='X'&&board[1][c]=='X'&&board[2][c]=='X')  win = 'X';
+    else if(board[0][c]=='O'&&board[1][c]=='O'&&board[2][c]=='O')  win = 'O';
+}
+
+    if(board[0][0]=='X'&&board[1][1]=='X'&&board[2][2]=='X')  win = 'X'; //1&5&9
+    else   if(board[0][0]=='O'&&board[1][1]=='O'&&board[2][2]=='O')  win = 'O';
+
+    if(board[0][2]=='X'&&board[1][1]=='X'&&board[2][0]=='X')  win = 'X'; //3&5&7
+    else   if(board[0][2]=='O'&&board[1][1]=='O'&&board[2][0]=='O')  win = 'O';
+
+if(win!='N') {
+print("\n!!!******!!!\n");
+printBoard(board);}
+    return win;
+}
+
 
 public static int[] getPair(int blockIndex)throws Exception{
     //this method to return pairs of given index for better UX.
